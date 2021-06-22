4e95c02839323ea06dde592f125816c8e927375a
‘13373338337@163.com’ <13373338337@163.com>
2021-06-22 19:34:22 +0800

    flink 滚动聚合算子

---
+++ Flink_study/src/main/java/com/amos/transform/TransForm_RollingAggregation.java
@@ -0,0 +1,71 @@
+package com.amos.transform;
+
+import com.amos.bean.CarSpeedInfo;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.functions.ReduceFunction;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.api.common.typeinfo.TypeHint;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
+
+import java.util.Properties;
+
+/**
+ * @Title: TransForm_RollingAggregation
+ * @Description: java类作用描述
+ * @Author: YuSong.Mu
+ * @Date: 2021/6/21 19:44
+ */
+public class TransForm_RollingAggregation {
+    public static void main(String[] args) throws Exception{
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.enableCheckpointing(5000);
+
+        Properties properties = new Properties();
+
+        properties.setProperty("bootstrap.servers", "hadoop01:9092,hadoop02:9092,hadoop03:9092");
+        properties.setProperty("group.id", "flink-kafka-002");
+        properties.setProperty("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        properties.setProperty("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        properties.setProperty("auto.offset", "latest");
+        DataStreamSource<String> source = env.addSource(new FlinkKafkaConsumer<String>("flink-kafka", new SimpleStringSchema(), properties));
+
+/*        source.map(new MapFunction<String, Tuple2<String,Long>>() {
+            @Override
+            public Tuple2<String,Long> map(String s) throws Exception {
+                String[] strings = s.split("\t");
+                String string = strings[0];
+                return new Tuple2<String, Long>(strings[1],Long.parseLong(strings[0]));
+            }
+        }).keyBy(0).maxBy(1).print();*/
+
+
+       //lambdas
+        source.map(line->{
+            String[] strings = line.split("\t");
+            CarSpeedInfo carSpeedInfo = new CarSpeedInfo(Long.parseLong(strings[0]),strings[1],strings[2],Integer.parseInt(strings[3]));
+
+            return carSpeedInfo;
+        }).keyBy("carId").max("speed").print();
+
+
+       /* source.map(line->{
+            String[] strings = line.split("\t");
+            CarSpeedInfo carSpeedInfo = new CarSpeedInfo(Long.parseLong(strings[0]),strings[1],strings[2],Integer.parseInt(strings[3]));
+
+            return carSpeedInfo;
+        }).keyBy("carId").reduce(new ReduceFunction<CarSpeedInfo>() {
+            @Override
+            public CarSpeedInfo reduce(CarSpeedInfo value1, CarSpeedInfo value2) throws Exception {
+
+                return new CarSpeedInfo(value1.getMonitorId(),value1.getCarId(),value2.getTimeStamp(),
+                        value1.getSpeed()>value2.getSpeed()?value1.getSpeed():value2.getSpeed());
+            }
+        }).print();*/
+        env.execute();
+
+    }
+}
