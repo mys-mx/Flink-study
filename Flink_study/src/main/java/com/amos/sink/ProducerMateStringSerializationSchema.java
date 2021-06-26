38dace6177c7d01e1259daa82e3cb3c963ad553e
‘13373338337@163.com’ <13373338337@163.com>
2021-06-26 10:23:50 +0800

    KafkaContextAware

---
+++ Flink_study/src/main/java/com/amos/sink/ProducerMateStringSerializationSchema.java
@@ -0,0 +1,37 @@
+package com.amos.sink;
+
+import org.apache.flink.streaming.connectors.kafka.KafkaContextAware;
+import org.apache.flink.streaming.connectors.kafka.KafkaSerializationSchema;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.nio.charset.StandardCharsets;
+
+/**
+ * @Title: ProducerStringSerializationSchema
+ * @Description: java类作用描述
+ * @Author: YuSong.Mu
+ * @Date: 2021/6/25 22:40
+ */
+public class ProducerMateStringSerializationSchema implements KafkaContextAware<String> {
+
+
+    @Override
+    public void setParallelInstanceId(int parallelInstanceId) {
+
+    }
+
+    @Override
+    public void setNumParallelInstances(int numParallelInstances) {
+
+    }
+
+    @Override
+    public void setPartitions(int[] partitions) {
+
+    }
+
+    @Override
+    public String getTargetTopic(String element) {
+        return null;
+    }
+}
