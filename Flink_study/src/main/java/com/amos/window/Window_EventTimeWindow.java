27023844f9f0c9cde0679c9b019df242ef128e81
13373338337@163.com <13373338337@163.com>
2021-07-02 20:09:58 +0800

    基于滚动时间窗口 测试侧输出流，允许最晚延迟时间

--- Flink_study/src/main/java/com/amos/window/Window_EventTimeWindow.java
+++ Flink_study/src/main/java/com/amos/window/Window_EventTimeWindow.java
@@ -7,10 +7,14 @@ import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.timestamps.BoundedOutOfOrdernessTimestampExtractor;
 import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
+import org.apache.flink.util.OutputTag;
+
+import java.io.OutputStream;
 
 /**
  * @Title: Window_EventTimeWindow
@@ -19,6 +23,7 @@ import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
 public class Window_EventTimeWindow {
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
         env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
         //设置watermark的周期时间
         env.getConfig().setAutoWatermarkInterval(100);
@@ -26,7 +31,7 @@ public class Window_EventTimeWindow {
         DataStreamSource<String> stringDataStreamSource = env.socketTextStream("hadoop01", 7777);
         DataStream<SensorReading> streamOperator = stringDataStreamSource.map(line -> {
             String[] strings = line.split(",");
-            return new SensorReading(strings[0], Long.parseLong(strings[1]), Double.parseDouble(strings[2]));
+            return new SensorReading(strings[0], new Long(strings[1]), new Double(strings[2]));
 
         })      /*
                     在assignTimestampsAndWatermarks中要做两件事：
@@ -40,11 +45,21 @@ public class Window_EventTimeWindow {
                             @Override //提取时间戳
                             public long extractTimestamp(SensorReading element) {
                                 //返回毫秒数
-                                return element.getTimeStamp() * 1000;
+                                return element.getTimeStamp() * 1000L;
                             }
                         });
 
+        OutputTag<SensorReading> outputTag = new OutputTag<SensorReading>("late") {
+        };
+        //基于事件时间的开窗聚合--->统计15秒内的温度的最小值
+        SingleOutputStreamOperator<SensorReading> minTempStream = streamOperator.keyBy("id")
+                .timeWindow(Time.seconds(15))
+                .allowedLateness(Time.minutes(1))
+                .sideOutputLateData(outputTag)
+                .minBy("temperature");
 
+        minTempStream.print("min temperature");
+        minTempStream.getSideOutput(outputTag).print("late");
         env.execute();
     }
 }
