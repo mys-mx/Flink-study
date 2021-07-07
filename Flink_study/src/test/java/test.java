2f5852b28f72bda52a45a6e15f124d453e9a4e38
13373338337@163.com <13373338337@163.com>
2021-07-07 22:10:02 +0800

    测试类

---
+++ Flink_study/src/test/java/test.java
@@ -0,0 +1,10 @@
+/**
+ * @program: Flink_study
+ * @description: 测试
+ * @create: 2021-07-07 22:06
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello flink");
+    }
+}
